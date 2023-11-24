a5b64d6c49716f63733e6b7327a646ac9ac86b72
Mohammod Arafat Mollik <arafathossain923@gmail.com>
2023-11-24 14:24:05 +0200

    Install chakra

--- app/page.jsx
+++ app/page.jsx
@@ -11,8 +11,7 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center  p-24">
       <h3 className="h-10"> Hi, you wanna plan a tour? Add bellow name of the tour and get started.</h3>
-      
-      <RevealDiv></RevealDiv>
+      <RevealDiv />
       <ShowTableForAllTours tours={tours} />
     </main>
   )
